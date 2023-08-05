edefdc621512dac8352be29fe4b742f5aaec556c
Aluno <nilao1256@gmail.com>
2023-08-05 11:06:32 -0300

    fix : ajuste busca de palavras

--- busca.js
+++ busca.js
@@ -5,9 +5,15 @@ const db = "Essa é a frase encontrada no banco de dados. Ola mundo";
 const palavra = document.querySelector('#search');
 
 function buscaPalavra(){
+  if(!palavra.value){
+    searchWord.innerHTML = 'Digite alguma palavra!';
+    return;
+  }
+
   if(db.toLowerCase().includes(palavra.value.toLowerCase())){
     searchWord.innerHTML = 'Essa palavra existe!';
     return;
   }
+  
    searchWord.innerHTML = 'Não existe essa palavra!';  
 }
