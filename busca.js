ed741aaf27468eafa1a87cab7f301afc69ac26f6
lucaspopov@protonmail.com <oLucasPopov>
2023-08-05 11:11:14 -0300

    docs: add documentation to buscarPalavra function

--- busca.js
+++ busca.js
@@ -4,6 +4,12 @@ const db = "Essa é a frase encontrada no banco de dados. Ola mundo";
 
 const palavra = document.querySelector('#search');
 
+/**
+ * A function that searches for a word and displays a message based on its existence.
+ *
+ * @param {string} palavra.value - The word to be searched.
+ * @return {undefined} No return value.
+ */
 function buscaPalavra(){
   if(!palavra.value){
     searchWord.innerHTML = 'Digite alguma palavra!';
